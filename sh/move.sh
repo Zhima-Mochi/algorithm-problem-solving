1dea9a6fc0c55066b6543fa25b6114ab99e99082
Zhima-Mochi <yehgoter@gmail.com>
2023-03-18 10:11:05 +0800

    "Renamed and moved files to corresponding directories" This commit renames and moves multiple files to their corresponding directories based on their file extensions. A new script is also added to automate the process.

---
+++ sh/move.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Loop through each file in the current directory
+for file in *; do
+    # Check if the file is actually a file (not a directory or other special file)
+    if [ -f "$file" ]; then
+        # Get the file extension
+        extension="${file##*.}"
+        # Check if there's a directory with the same name as the extension
+        if [ -d "$extension" ]; then
+            # Move the file to the corresponding directory
+            mv "$file" "$extension"
+        else
+            # If there's no directory with the same name as the extension, create one and move the file
+            mkdir "$extension"
+            mv "$file" "$extension"
+        fi
+    fi
+done
